3f58a49c80026f0b752a0d1de21e0f5bf57e98b6
Hugo Reyes <hugoreyes@hugos-mbp.mynetworksettings.com>
2024-01-28 14:25:00 -0500

    changes to layout to include the text field and button

--- chatcito/src/App.js
+++ chatcito/src/App.js
@@ -1,46 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Grid from '@mui/material/Grid'; // Import Grid
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import ButtonAppBar from './ButtonAppBar';
 import AlignItemsList from './AlignItemsList';
-import VirtualizedList from './VirtualizedList';
-import { MessageList } from "react-chat-elements"
-import "react-chat-elements/dist/main.css"
+import { MessageList } from "react-chat-elements";
+import "react-chat-elements/dist/main.css";
+import messagesHardcoded from './messagesModel'; // Import messages
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages(messagesHardcoded);
+  }, []);
+
+  const handleSend = () => {
+    const newMessage = {
+      position: "left",
+      type: "text",
+      title: "Kursat",
+      text: inputText,
+    };
+
+    setMessages((prevMessages) => [...prevMessages, newMessage]);
+    setInputText('');
+  };
+
   return (
     <div className="App">
-      <ButtonAppBar/>
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <AlignItemsList/>
+      <ButtonAppBar />
+      <AlignItemsList />
       <MessageList
-    className='message-list'
-    lockable={true}
-    toBottomHeight={'40%'}
-    dataSource={[
-    {
-      position:"left",
-      type:"text",
-      title:"Kursat",
-      text:"Give me a message list example !",
-    },
-    {
-      position:"right",
-      type:"text",
-      title:"Emre",
-      text:"That's all.",
-    },
-    ]}
-/>
+        className='message-list'
+        lockable={true}
+        toBottomHeight={'40%'}
+        dataSource={messages}
+      />
+      <Grid container spacing={2} alignItems="center" className="message-input">
+        <Grid item xs>
+          <TextField
+            id="outlined-basic"
+            label="Type a message"
+            variant="outlined"
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            fullWidth
+          />
+        </Grid>
+        <Grid item>
+          <Button variant="contained" onClick={handleSend}>Send</Button>
+        </Grid>
+      </Grid>
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
---
+++ chatcito/src/messagesModel.js
@@ -0,0 +1,18 @@
+// messagesModel.js
+const messagesHardcoded = [
+    {
+      position: "left",
+      type: "text",
+      title: "Kursat",
+      text: "Give me a message list example!",
+    },
+    {
+      position: "right",
+      type: "text",
+      title: "Emre",
+      text: "That's all.",
+    },
+  ];
+  
+  export default messagesHardcoded;
+  
