5fec7e80243c5588d5b81f4b3432985346d4ff4c
Hugo Reyes <hugoreyes@hugos-mbp.mynetworksettings.com>
2024-01-29 22:29:35 -0500

    added onKeyDown to send new message when user press enter

--- chatcito/src/App.js
+++ chatcito/src/App.js
@@ -17,6 +17,11 @@ function App() {
   }, []);
 
   const handleSend = () => {
+    if (inputText.trim() === '') {
+      // Prevent sending empty messages
+      return;
+    }
+
     const newMessage = {
       position: "right", // Assuming you want the sent message to appear on the right
       type: "text",
@@ -28,20 +33,32 @@ function App() {
     setInputText('');
   };
 
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault(); // Prevents the default action of the enter key
+      handleSend();
+    }
+  };
+
   const inputHeight = 60; // Adjust the height to match your TextField and Button size
+  const appBarHeight = 64;
 
   return (
-    <div className="App" style={{ paddingBottom: `${inputHeight}px` }}>
-      <ButtonAppBar />
-      <AlignItemsList />
-      <div style={{ overflowY: 'auto', height: `calc(100vh - ${inputHeight}px - /* height of ButtonAppBar */)` }}>
-        <MessageList
-          className='message-list'
-          lockable={true}
-          toBottomHeight={'100%'}
-          dataSource={messages}
-        />
+    <div className="App">
+      <ButtonAppBar style={{ position: 'fixed', top: 0, left: 0, right: 0, zIndex: 1000 }} />
+      
+      <div style={{ paddingTop: `${appBarHeight}px` }}>
+        <AlignItemsList />
+        <div style={{ overflowY: 'auto', height: `calc(100vh - ${inputHeight}px - ${appBarHeight}px)` }}>
+          <MessageList
+            className='message-list'
+            lockable={false}
+            toBottomHeight={'100%'}
+            dataSource={messages}
+          />
+        </div>
       </div>
+
       <div style={{ position: 'fixed', bottom: 0, left: 0, right: 0, height: `${inputHeight}px`, background: '#fff', borderTop: '1px solid #ccc' }}>
         <Grid container spacing={0} alignItems="center" className="message-input">
           <Grid item xs>
@@ -50,12 +67,13 @@ function App() {
               label="Type a message"
               variant="outlined"
               value={inputText}
+              onKeyDown={handleKeyDown}
               onChange={(e) => setInputText(e.target.value)}
               fullWidth
             />
           </Grid>
           <Grid item>
-            <Button variant="contained" onClick={handleSend}>Send</Button>
+            <Button variant="contained" onClick={handleSend} style={{ height: `${inputHeight}px` }}>Send</Button>
           </Grid>
         </Grid>
       </div>
